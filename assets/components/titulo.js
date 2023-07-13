e3e94186de5d08d1d780af37fb156f16f33018e4
emnuelle <rm97973@fiap.com.br>
2023-07-13 11:22:25 -0300

    atualizações

--- assets/components/titulo.js
+++ assets/components/titulo.js
@@ -1,17 +1,25 @@
-import { LitElement, html, css } from 'lit';
+import { LitElement, html, css } from "lit";
 
 export class titulo extends LitElement {
-    static styles = [
-        css`
+  static styles = [
+    css`
             :host {
-                display: block
+                display: block;
 
+                color: var(--cor-secundaria, #665E57);
+
+                text-shadow: 2px 2px 0px 0px rgba(0, 0, 0, 0.25);
+                font-family: Exo;
+                font-size: 48px;
+                font-style: normal;
+                font-weight: 700;
+                line-height: 100%; /* 48px */
             }
-        `
-    ];
+        `,
+  ];
 
-    render() {
-        return html `Título`;
-    }
+  render() {
+    return html`Título`;
+  }
 }
-customElements.define('app-titulo', titulo);
+customElements.define("app-titulo", titulo);
