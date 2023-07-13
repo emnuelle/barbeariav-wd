310f41f78190900087b24966fdb760a5b22dc59b
emnuelle <rm97973@fiap.com.br>
2023-07-13 11:39:16 -0300

    Ajustando as fontes

--- assets/components/titulo.js
+++ assets/components/titulo.js
@@ -8,8 +8,8 @@ export class titulo extends LitElement {
 
                 color: var(--cor-secundaria, #665E57);
 
-                text-shadow: 2px 2px 0px 0px rgba(0, 0, 0, 0.25);
-                font-family: Exo;
+                text-shadow: 2px 2px 0px rgba(0, 0, 0, 0.25);
+                font-family: var(--fonte-titulo);
                 font-size: 48px;
                 font-style: normal;
                 font-weight: 700;
