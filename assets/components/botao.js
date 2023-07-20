690f649640fad743ec895d9da0e70120ca947447
emnuelle <rm97973@fiap.com.br>
2023-07-20 11:36:16 -0300

    adicionando a parte de contatos

--- assets/components/botao.js
+++ assets/components/botao.js
@@ -1,22 +1,19 @@
 import { LitElement, html, css } from "lit";
 
 export class Botao extends LitElement {
-
   static styles = [
-
     css`
-
       :host {
         display: block;
       }
 
       button {
         width: inherit;
+
         display: inline-flex;
         padding: 5px 24px;
         border: 0;
         justify-content: flex-start;
-
         align-items: center;
 
         border-radius: 8px;
@@ -24,15 +21,14 @@ export class Botao extends LitElement {
           --button-background,
           color-mix(in srgb, white 20%, var(--tom-2, #b38962))
         );
-
         box-shadow: 0px 2px 4px 0px #665e57;
 
         /* Tipografia */
+
         color: var(
           --color,
           color-mix(in srgb, black 20%, var(--tom-3, #664e38))
         );
-
         text-align: center;
         font-family: var(--fonte-titulo);
         font-size: 1rem;
@@ -40,9 +36,9 @@ export class Botao extends LitElement {
         font-weight: 700;
         line-height: normal;
         white-space: nowrap;
+
         transition: all 0.2ms;
         cursor: pointer;
-
       }
 
       button:hover,
@@ -60,9 +56,7 @@ export class Botao extends LitElement {
         height: 24px;
         margin: 5px 8px 5px 0;
       }
-
     `,
-
   ];
 
   render() {
@@ -70,6 +64,5 @@ export class Botao extends LitElement {
       <button><slot name="icone"></slot> <slot> Pressione</slot></button>
     `;
   }
-
 }
 customElements.define("app-botao", Botao);
